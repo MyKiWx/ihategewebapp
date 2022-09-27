40245b7f9ee38504b2121f6ea869e25a1b71aab1
MyKiWx <88845004+MyKiWx@users.noreply.github.com>
2022-09-28 00:39:30 +0700

    upload first version of ge web app 7

--- service/firebase.config.ts
+++ service/firebase.config.ts
@@ -3,13 +3,11 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvPqLD5UajvEBZipZEs0OSIml3M5udQ50",
-  authDomain: "fir-ge-c7fa7.firebaseapp.com",
-  projectId: "fir-ge-c7fa7",
-  storageBucket: "fir-ge-c7fa7.appspot.com",
-  messagingSenderId: "117029851362",
-  appId: "1:117029851362:web:754867d750c4a7c7bbcebb",
-  measurementId: "G-J3QTTL0R31"
+
+
+  // Your api
+
+
 };
 
 // Initialize Firebase
